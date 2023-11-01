38d67e6e1e3bb696c800076012996cd698b42b2c
koudaihirata <2230051@ecc.ac.jp>
2023-11-01 09:10:00 +0900

    本田に追い付いた

--- index.js
+++ index.js
@@ -12,8 +12,13 @@ sendBtn.addEventListener( "click", () =>{
     // 作成したdiv要素に"comment"クラスを追加
     comment.classList.add("comment");
 
-    // 作成したdiv要素の内部HTMLにテキストボックスの値を設定
-    comment.textContent = textBox.value;
+    // 作成したdiv要素の内部HTMLにテキストボックスの値と時刻を設定
+    let time = new Date();
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
+    let Time = "   " + hours + ":" + minutes + ":" + seconds
+    comment.innerHTML = textBox.value + "<small>" + Time + "</small>";
 
     // 親divの中に子divを作成
     let CDelete = document.createElement("div");
@@ -26,7 +31,6 @@ sendBtn.addEventListener( "click", () =>{
     // コメントエリア内でランダムな位置を計算
     let randomLeft = Math.floor(Math.random() * (commentArea.offsetWidth - 300));
     let randomTop = Math.floor(Math.random() * (commentArea.offsetHeight - 120));
-
     // 計算したランダムな位置をコメントに適用
     comment.style.top = randomTop + "px";
     comment.style.left = randomLeft + "px";
@@ -35,25 +39,64 @@ sendBtn.addEventListener( "click", () =>{
     commentArea.appendChild(comment);
 
 
-// ".comment"というクラスを持つすべての要素を選択し、それらを"comments"という変数に格納します。
+
+    // ".comment"クラスを持つすべての要素を選択し、それらを"comments"変数に格納します。
     let comments = document.querySelectorAll(".comment");
 
-    // "comments"変数に格納された各要素（コメント）に対して、次の操作を行います。
-    comments.forEach((comment) => {
-        // 各コメント要素に対して、"dblclick"（ダブルクリック）イベントリスナーを追加します。
-        comment.addEventListener("dblclick", () => {
-            // ダブルクリックされたコメント要素に"disappear"というクラスを追加します。
-            // これにより、CSSで定義された"disappear"クラスのスタイルが適用され、コメントが表示から消える（または他の効果が適用される）ことでしょう。
-            comment.classList.add("disappear");
-            
-            // 各コメント要素に対して、"animationend"（アニメーション終了）イベントリスナーを追加します。
-            comment.addEventListener("animationend", () => {
-                // アニメーションが終了したら、コメント要素を完全に削除します。
-                comment.remove();
-            });
+    // "flag"変数を初期化します。これはマウスが要素上で押されているかどうかを追跡します。
+    let flag = false;
+    let zIndex = 0; //z-indexの初期値を設定
+
+    // "comments"の各要素に対して処理を行います。
+    comments.forEach((comment)=>{
+        // "mousedown"イベントリスナーを追加します。これはマウスボタンが要素上で押されたときに発火します。
+        comment.addEventListener("mousedown",()=>{
+            // マウスボタンが押されたら、"flag"をtrueに設定します。
+            flag = true;
+            comment.style.zIndex = ++zIndex;// mousedownイベントが発生したら、その要素のz-indexを増やす
+        })
+        // "mouseout"イベントリスナーを追加します。これはマウスカーソルが要素から離れたときに発火します。
+        comment.addEventListener("mouseup",()=>{
+            // マウスカーソルが要素から離れたら、"flag"をfalseに設定します。
+            flag = false;
+        })
+
+        // "mousemove"イベントリスナーを追加します。これはマウスカーソルが要素上で移動したときに発火します。
+        comment.addEventListener("mousemove",(e)=>{
+            // "flag"がtrue（つまり、マウスボタンが押されている）場合のみ、以下の処理を行います。
+            if (flag) {
+                // 要素の現在の位置とサイズに関する情報を取得します。
+                let rect = comment.getBoundingClientRect();
+                // 要素の左端の位置を、マウスカーソルのX座標から要素の幅の半分を引いた値に設定します。
+                comment.style.left = (e.pageX - rect.width / 2)+ 'px';
+                // 要素の上端の位置を、マウスカーソルのY座標から要素の高さの半分を引いた値に設定します。
+                comment.style.top = (e.pageY - rect.height / 2) + 'px';
+            }
+        })
+    })
+
+
+
+// ".comment"クラスを持つすべての要素を選択し、それらを"comments"変数に格納します。
+comments.forEach((comment) => {
+    // 各"comment"内から".delete"クラスを持つ要素（ここでは削除ボタンと仮定）を選択し、それを"Delete"変数に格納します。
+    let Delete = comment.querySelector('.delete'); 
+
+    // "Delete"に対してクリックイベントリスナーを追加します。これはボタンがクリックされたときに発火します。
+    Delete.addEventListener("click", () => { 
+        // ボタンがクリックされたら、その親要素である"comment"に"disappear"クラスを追加します。
+        comment.classList.add("disappear");
+
+        // "comment"に対してアニメーション終了イベントリスナーを追加します。これはCSSアニメーションが終了したときに発火します。
+        comment.addEventListener("animationend", () => {
+            // アニメーションが終了したら、"comment"要素自体を削除します。
+            comment.remove();
         });
-    });    
+    });
+});
     
+    
+
 });
 
 
